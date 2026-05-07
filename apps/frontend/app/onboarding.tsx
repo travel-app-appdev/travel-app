d31976cbb563f5d589193813265127079ff298ca
IlvieRath <ilvierath0408@gmail.com>
2026-05-07 02:45:45 +0200

    Base for different Pages

--- apps/frontend/app/onboarding.tsx
+++ apps/frontend/app/onboarding.tsx
@@ -14,6 +14,10 @@ import {
   Text
 } from "react-native";
 import { colors, spacing, radius, typography } from "@/src/theme";
+import { AppButton } from "@/src/components/common/AppButton";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+
 
 const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
@@ -25,47 +29,101 @@ export default function Onboarding() {
             <View style={Styles.safeArea}>
                 <View style={Styles.root}>
                     {step === 1 ? (
-                        <>
-                    <View style={Styles.header}>
-                        <View
-                      style={Styles.headerTitle}
-                      accessible={false}
-                      importantForAccessibility="no-hide-descendants"
-                    >
-                      <Plane width={25} height={25} />
-                        <Text style={Styles.headerLabel}>Onboarding</Text>
-                        </View>
-                    </View>
 
-                    <View style={Styles.container}>
-                        <Text style={[Styles.title, Styles.titleBg]}>Planning Together</Text>
-                    </View>
-                    </>
-                    ) : step === 2 ? (
-
+                    <>
                         <View style={Styles.header}>
-                        <View
-                      style={Styles.headerTitle}
-                      accessible={false}
-                      importantForAccessibility="no-hide-descendants"
-                    >
-                      <Plane width={25} height={25} />
-                        <Text style={Styles.headerLabel}>Onboarding</Text>
+                            <View
+                        style={Styles.headerTitle}
+                        accessible={false}
+                        importantForAccessibility="no-hide-descendants"
+                        >
+                        <Plane width={25} height={25} />
+                            <Text style={Styles.headerLabel}>Onboarding</Text>
+                            </View>
                         </View>
-                    </View>
+
+                        <View style={Styles.container}>
+                            <Text style={[Styles.title, Styles.titleBg1]}>Plan Together</Text>
+                        </View>
+
+                        <View style={[Styles.continueWrapper, { pointerEvents: "box-none" }]}>
+                            <AppButton
+                                title="Continue"
+                                onPress={() => setStep(2)}
+                                style={Styles.continueButton}
+                                textStyle={Styles.continueButtonText}
+                                accessibilityLabel="Continue to next step"
+                                accessibilityHint="Moves to the next step of onboarding"
+                                        />
+                        </View>
+                    </>
+
+                    ) : step === 2 ? (
+                    <>
+                        <View style={Styles.header}>
+                            <View
+                                style={Styles.headerTitle}
+                                accessible={false}
+                                importantForAccessibility="no-hide-descendants"
+                                >
+                                <Plane width={25} height={25} />
+                                <Text style={Styles.headerLabel}>Onboarding</Text>
+                            </View>
+                        </View>
+
+                        <View style={Styles.container}>
+                            <Text style={[Styles.title, Styles.titleBg2]}>Decide Together</Text>
+                        </View>
+
+                        <View style={[Styles.continueWrapper, { pointerEvents: "box-none" }]}>
+                            <AppButton
+                                title="Previous"
+                                onPress={() => setStep(1)}
+                                style={Styles.continueButton}
+                                textStyle={Styles.continueButtonText}
+                                accessibilityLabel="Go to Previous step"
+                                accessibilityHint="Moves to the previous step of onboarding"
+                            />
+                            <AppButton
+                                title="Continue"
+                                onPress={() => setStep(3)}
+                                style={Styles.continueButton}
+                                textStyle={Styles.continueButtonText}
+                                accessibilityLabel="Continue to next step"
+                                accessibilityHint="Moves to the next step of onboarding"
+                            />
+                        </View>
+                    </>
 
                     ) : (
 
+                    <>
                         <View style={Styles.header}>
-                        <View
-                      style={Styles.headerTitle}
-                      accessible={false}
-                      importantForAccessibility="no-hide-descendants"
-                    >
-                      <Plane width={25} height={25} />
-                        <Text style={Styles.headerLabel}>Onboarding</Text>
+                            <View
+                            style={Styles.headerTitle}
+                            accessible={false}
+                            importantForAccessibility="no-hide-descendants"
+                            >
+                                <Plane width={25} height={25} />
+                                <Text style={Styles.headerLabel}>Onboarding</Text>
+                            </View>
                         </View>
-                    </View>
+
+                        <View style={Styles.container}>
+                            <Text style={[Styles.title, Styles.titleBg3]}>Travel Together</Text>
+                        </View>
+
+                        <View style={[Styles.continueWrapper, { pointerEvents: "box-none" }]}>
+                            <AppButton
+                                title="Previous"
+                                onPress={() => setStep(2)}
+                                style={Styles.continueButton}
+                                textStyle={Styles.continueButtonText}
+                                accessibilityLabel="Go to Previous step"
+                                accessibilityHint="Moves to the previous step of onboarding"
+                            />
+                        </View>
+                    </>
                     )
                 }
                 </View>
@@ -112,11 +170,23 @@ const Styles = StyleSheet.create({
       textAlign: "center",
       alignSelf: "stretch",
     },
-    titleBg: {
+    titleBg1: {
     borderRadius: radius.md,
     padding: spacing.md,
     gap: spacing.sm,
     backgroundColor: colors.beachYellow
+    },
+    titleBg2: {
+    borderRadius: radius.md,
+    padding: spacing.md,
+    gap: spacing.sm,
+    backgroundColor: colors.sunsetPink
+    },
+    titleBg3: {
+    borderRadius: radius.md,
+    padding: spacing.md,
+    gap: spacing.sm,
+    backgroundColor: colors.neonGreen
     },
     container: {
     paddingHorizontal: spacing.xl,
@@ -129,5 +199,22 @@ const Styles = StyleSheet.create({
     textAlign: "center",
     paddingTop: spacing.xxxxl2,
   },
-
+  continueWrapper: {
+    position: "absolute",
+    bottom: SCREEN_WIDTH * (140 / 393),
+    left: spacing.xl,
+    right: spacing.xl,
+    zIndex: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    },
+  continueButton: {
+    backgroundColor: colors.sunsetOrange,
+    flex: 1, 
+    marginRight: spacing.sm
+  },
+  continueButtonText: {
+    color: colors.nightBlack,
+    fontFamily: typography.fontFamily.bodyBold,
+  },
 });
